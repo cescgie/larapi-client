48698cd69a9f239920b3e66d3f5b9c6b9b34b4ac
cescgie <yoggifirmanda@gmail.com>
2016-03-17 14:57:33 -0400

    move api into client

--- vendor/satellizer.js
+++ vendor/satellizer.js
@@ -21,7 +21,7 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
       httpInterceptor: function() { return true; },
       withCredentials: false,
       tokenRoot: null,
-      baseUrl: 'http://46.101.155.176/',
+      baseUrl: '/api/public/',
       loginUrl: '/auth/login',
       signupUrl: '/auth/signup',
       unlinkUrl: '/auth/unlink/',
