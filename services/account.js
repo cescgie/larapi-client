9617e2bb7b77bc2ac3b4d51de6fb6c86a311fd4f
cescgie <yoggifirmanda@gmail.com>
2016-03-17 15:28:50 -0400

    client route to api

--- services/account.js
+++ services/account.js
@@ -2,10 +2,10 @@ angular.module('MyApp')
   .factory('Account', function($http) {
     return {
       getProfile: function() {
-        return $http.get('api/public/api/me');
+        return $http.get('api/api/me');
       },
       updateProfile: function(profileData) {
-        return $http.put('api/public/api/me', profileData);
+        return $http.put('api/api/me', profileData);
       }
     };
   });
--- vendor/satellizer.js
+++ vendor/satellizer.js
@@ -21,7 +21,7 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
       httpInterceptor: function() { return true; },
       withCredentials: false,
       tokenRoot: null,
-      baseUrl: '/api/public/',
+      baseUrl: '/api/',
       loginUrl: '/auth/login',
       signupUrl: '/auth/signup',
       unlinkUrl: '/auth/unlink/',
